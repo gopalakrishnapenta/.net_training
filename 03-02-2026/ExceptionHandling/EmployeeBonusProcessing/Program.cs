8317ba297643b6047ebff1381e05be5467278b4b
Gopala Krishna <gopalkrishnapenta@gmail.com>
2026-02-03 15:40:48 +0530

    updated

---
+++ 03-02-2026/ExceptionHandling/EmployeeBonusProcessing/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+class BonusCalculator
+{
+    public static void Main()
+    {
+        int[] salaries = {5000, 0 , 7000, 6000};
+        int bonus = 10000;
+
+        for (int i=0;i<salaries.Length;i++)
+{
+            try
+            {
+                int result = bonus/salaries[i];
+                Console.WriteLine($"Employee{i+1}: Bonus share = {result}");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine($"Employee{i+1}: Salary cannot be zero.");
+            }
+        }  
+        Console.WriteLine("Bonus distribution completed.");  
+    }
+}
---
+++ 03-02-2026/ExceptionHandling/ExceptionPropagationAcrossLayers/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Controller
+{
+    static void Main()
+    {
+        try
+        {
+            // Call Service method
+            Service.Process();
+        }
+        catch (Exception ex)
+        {
+            // Handle exception at top layer
+            Console.WriteLine("Controller handled exception: " + ex.Message);
+        }
+    }
+}
+
+class Service
+{
+    public static void Process()
+    {
+        try
+        {
+            // Call Repository method
+            Repository.GetData();
+        }
+        catch (Exception ex)
+        {
+            // Catch and log exception
+            Console.WriteLine("Service log: " + ex.Message);
+
+            // Rethrow exception to Controller
+            throw;
+        }
+    }
+}
+
+class Repository
+{
+    public static void GetData()
+    {
+        // Throw exception at data layer
+        throw new Exception("Database connection failed.");
+    }
+}

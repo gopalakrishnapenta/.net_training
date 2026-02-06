f8e42bd0610e68a1d71345271a87e071e8f4fc6d
Gopala Krishna <gopalkrishnapenta@gmail.com>
2026-02-06 12:13:00 +0530

    Updated

--- MYConsoleApp/obj/Debug/net10.0/MyConsoleApp.AssemblyInfo.cs
+++ MYConsoleApp/obj/Debug/net10.0/MyConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbc01d1aaf17733700126d8dce6cb25e69c47bca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc986a04452e9362d78e6cafbfcdb533c54ff1b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
